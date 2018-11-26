cf20c188c3764dc0af8e5a8b1ed1acc07cc35c2a
jimmy ibanez <jimmy.ibanez@outlook.com>
2018-11-26 00:57:19 -0300

    Integrated Axios

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
   onSearchSubmit = term => {
-    console.log(term);
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization:
+          'Client-ID dcca6e5b968924bd517259f8e3a64408b4a30dbc3c733930bee8ac75a5b1612b'
+      }
+    });
   };
 
   render() {
