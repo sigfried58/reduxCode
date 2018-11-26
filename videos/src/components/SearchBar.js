0f2299eb256771751b2e54be515e7aad119eb4af
jimmy ibanez <jimmy.ibanez@outlook.com>
2018-11-26 18:13:41 -0300

    Updating state with fetched data

--- videos/src/components/SearchBar.js
+++ videos/src/components/SearchBar.js
@@ -1,10 +1,7 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { term: '' };
-  }
+  state = { term: '' };
 
   onInputChange = event => {
     this.setState({ term: event.target.value });
