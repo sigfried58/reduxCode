da66acde5534ab5b89c983eceaa501e7964eeb0e
jimmy ibanez <jimmy.ibanez@outlook.com>
2018-11-26 18:44:51 -0300

    Rendering video thumbnails

---
+++ videos/src/components/VideoList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import VideoItem from './VideoItem';
+
+const VideoList = ({ videos }) => {
+  const renderedList = videos.map(video => {
+    return <VideoItem key={video.id.videoId} video={video} />;
+  });
+
+  return <div>{renderedList}</div>;
+};
+
+export default VideoList;
