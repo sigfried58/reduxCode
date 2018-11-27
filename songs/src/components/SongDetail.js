b70edc5e31696f1127216ed19bbfdd78d3d2df43
jimmy ibanez <jimmy.ibanez@outlook.com>
2018-11-27 18:28:50 -0300

    Conditional rendering

--- songs/src/components/SongDetail.js
+++ songs/src/components/SongDetail.js
@@ -2,7 +2,19 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 const SongDetail = ({ song }) => {
-  return <div>{song ? song.title : null}</div>;
+  if (!song) {
+    return <div>Select a song</div>;
+  }
+  return (
+    <div>
+      <h3>Details for:</h3>
+      <p>
+        Title: {song.title}
+        <br />
+        Duration: {song.duration}
+      </p>
+    </div>
+  );
 };
 
 const mapStateToProps = state => {
