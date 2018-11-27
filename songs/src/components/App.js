9ce309110e1bcaa97d38d80d9fe4c21adb88782f
jimmy ibanez <jimmy.ibanez@outlook.com>
2018-11-27 15:06:11 -0300

    Building actions

--- songs/src/components/App.js
+++ songs/src/components/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
+import { selectSong } from '../actions';
 
-class App extends React.Component {
-  render() {
-    return <div>App</div>;
-  }
-}
+const App = () => {
+  return <div>App</div>;
+};
 
 export default App;
