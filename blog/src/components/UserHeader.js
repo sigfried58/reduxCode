c8e1a32eebd416dfe02de95311d962b27f2c2dde
jimmy ibanez <jimmy.ibanez@outlook.com>
2018-11-28 18:23:53 -0300

    Finding relevant users

---
+++ blog/src/components/UserHeader.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { fetchUser } from '../actions';
+
+class UserHeader extends React.Component {
+  componentDidMount() {
+    this.props.fetchUser(this.props.userId);
+  }
+
+  render() {
+    console.log(this.props.users);
+    const user = this.props.users.find(user => user.id === this.props.userId);
+
+    if (!user) {
+      return null;
+    }
+
+    return <div className="header">{user.name}</div>;
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    users: state.users
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { fetchUser }
+)(UserHeader);
