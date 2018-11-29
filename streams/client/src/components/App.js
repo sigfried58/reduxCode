487376bdde31ab1c44217b1d472d725a3a52ccb8
jimmy ibanez <jimmy.ibanez@outlook.com>
2018-11-29 15:36:13 -0300

    Always visible components

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -10,6 +10,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
+        <h1>Header!</h1>
         <BrowserRouter>
           <div>
             <Route path="/" exact component={StreamList} />
