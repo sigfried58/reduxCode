17defe4c97df3494813d40da84607b0ff871af7b
jimmy ibanez <jimmy.ibanez@outlook.com>
2018-11-29 16:19:15 -0300

    Links inside routers

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -10,10 +10,10 @@ import StreamShow from './streams/StreamShow';
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <Header />
+      <div className="ui container">
         <BrowserRouter>
           <div>
+            <Header />
             <Route path="/" exact component={StreamList} />
             <Route path="/streams/new" exact component={StreamCreate} />
             <Route path="/streams/edit" exact component={StreamEdit} />
